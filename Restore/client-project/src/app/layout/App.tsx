d040dc2c15a2e84966bd64af171733f1a96fc4c0
ThePowerSerg <sferreir@bu.edu>
2025-10-10 10:13:33 -0400

    navbar addition

--- Restore/client-project/src/app/layout/App.tsx
+++ Restore/client-project/src/app/layout/App.tsx
@@ -1,39 +1,44 @@
 import { useEffect, useState } from "react"
 import type { Product } from "../models/product";
 import Catalog from "../../features/catalog/Catalog";
-import { Box, Button, Container, Typography } from "@mui/material";
+import { Box, Container, createTheme, CssBaseline, ThemeProvider } from "@mui/material";
+import NavBar from "./NavBar";
 
 function App() {
 
- const [products, setProducts] = useState<Product[]>([]);
+  const [products, setProducts] = useState<Product[]>([]);
 
- useEffect(() => {
-  fetch('http://localhost:5294/api/products')
-  .then(response => response.json())
-  .then(data => setProducts(data))  
- },[])
+  const darkModeToggle = false;
+  const palleteType = darkModeToggle ? 'dark' : 'light'
 
-  const addProduct = () => {
-    setProducts (previousState => [...previousState, {
-      id: previousState.length + 1
-      ,name: 'product ' + (previousState.length)
-      ,price: previousState.length * 100
-      ,quantityInStock: 100
-      ,description: 'test'
-      ,pictureUrl: 'https://test.com'
-      ,type:'test'
-      ,brand:'test'
-    }])
-  }
+  const darkModeTheme = createTheme({
+    palette: {
+      mode: palleteType,
+      background: {
+        default: (palleteType === 'light') ? '#eaeaea' : '#121212'
+      }
+    }
+  })
+
+  useEffect(() => {
+    fetch('http://localhost:5294/api/products')
+      .then(response => response.json())
+      .then(data => setProducts(data))
+  }, [])
 
   return (
-    <Container maxWidth='xl'>
-      <Box display='flex' justifyContent= 'center' gap={3} marginY={3}>
-        <Typography variant='h4'>ReStore</Typography>
-        <Button variant='contained' onClick={addProduct}>Submit</Button>
+    <ThemeProvider theme={darkModeTheme}>
+      <CssBaseline/>
+      <NavBar />
+      <Box sx={{
+          minHeight:'100vh',
+          background: darkModeToggle ? '#121212' : '#eaeaea'
+      }}>
+        <Container maxWidth='xl' sx={{ mt: 14 }}>
+          <Catalog productsList={products} />
+        </Container>
       </Box>
-      <Catalog productsList={products} />
-    </Container>
+    </ThemeProvider>
   )
 }
 
