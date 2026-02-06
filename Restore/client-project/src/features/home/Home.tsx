45fc3d2b1d0838277a007944231b330f866905ed
ThePowerSerg <sferreir@bu.edu>
2026-02-06 17:39:57 -0500

    add hook details

--- Restore/client-project/src/features/home/Home.tsx
+++ Restore/client-project/src/features/home/Home.tsx
@@ -8,11 +8,19 @@ interface Person {
 export default function Home() {
 
   const [person, setPerson] = useState<Person>({name:'Sergio'});
-  
+
+  // const handleClick: Person = () => {
+  //   setPerson({
+  //     ...person,
+  //     name: 'Luis!'
+  //   })
+  // }
+
   return (
     <>
     <div>{person.name}</div>
-    <Button color="error" onClick={() => setPerson({ ...person, name: 'Jane Smith' })}>Update Inline</Button>
+    <Button color="error" onClick={() => setPerson({ ...person, name: 'Inline name' })}>Update Inline</Button>
+    {/* <Button color="error" onClick={handleClick}>Update</Button> */}
     </>
   )
 }
