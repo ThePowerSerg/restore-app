7d1b2b8d635ec2edfdf2c6972fab3571f28af4e2
ThePowerSerg <sferreir@bu.edu>
2026-08-19 14:17:02 -0400

    Add config changes and wwwroot folder

--- Restore/client-project/vite.config.ts
+++ Restore/client-project/vite.config.ts
@@ -4,7 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   build: {
-    outDir: '../API/wwwroot'
+    outDir: '../API/wwwroot',
+    chunkSizeWarningLimit: 1024,
+    emptyOutDir: true
   },
   plugins: [react()],
 })
