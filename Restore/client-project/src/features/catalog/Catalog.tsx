c682994095018c8817890fd611b6301891f7db98
ThePowerSerg <sferreir@bu.edu>
2026-08-19 09:02:59 -0400

    remove old comment

--- Restore/client-project/src/features/catalog/Catalog.tsx
+++ Restore/client-project/src/features/catalog/Catalog.tsx
@@ -4,23 +4,13 @@ import { useFetchProductsQuery } from "./CatalogApi";
 import ProductList from "./ProductList";
 
 export default function Catalog() {
+  const { data, isLoading } = useFetchProductsQuery();
 
-    const {data, isLoading} = useFetchProductsQuery();
+  if (isLoading || !data) return <div>Loading...</div>;
 
-    if (isLoading || !data) return <div>Loading...</div>
-
-    // Original design
-    // const [products, setProducts] = useState<Product[]>([]);
-    
-    // useEffect(() => {
-    //     fetch('http://localhost:5294/api/products')
-    //       .then(response => response.json())
-    //       .then(data => setProducts(data))
-    //   }, [])
-
-    return (
-        <>
-            <ProductList productsforCards={data} />
-        </>
-    )
+  return (
+    <>
+      <ProductList productsforCards={data} />
+    </>
+  );
 }
