5235b7c786e34f116260380258ba7ad5fa58242d
ThePowerSerg <sferreir@bu.edu>
2026-08-19 12:52:23 -0400

    Add exception handling endpoint

--- Restore/API/Controllers/ProductsController.cs
+++ Restore/API/Controllers/ProductsController.cs
@@ -1,23 +1,16 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using API.Data;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
-using Microsoft.CodeAnalysis;
 
 namespace API.Controllers
 {
-    [Route("api/[controller]")] // https://localhost:5001/api/products
-    [ApiController]
-    public class ProductsController (StoreContext context): ControllerBase
+    public class ProductsController(StoreContext context) : BaseApiController
     {
         [HttpGet]
         public ActionResult<List<Product>> GetProducts()
         {
             return context.Products.ToList();
-        }  
+        }
 
         [HttpGet("{id}")]
         public ActionResult<Product> GetProducts(int id)
@@ -27,6 +20,6 @@ namespace API.Controllers
             if (product == null) return NotFound();
 
             return product;
-        }  
+        }
     }
 }
