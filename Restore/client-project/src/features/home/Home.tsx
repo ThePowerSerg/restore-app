7c166ee6bf0da86bf751983e6008b9385ab7bada
ThePowerSerg <sferreir@bu.edu>
2026-08-19 14:05:02 -0400

    Add home page and config

--- Restore/client-project/src/features/home/Home.tsx
+++ Restore/client-project/src/features/home/Home.tsx
@@ -1,26 +1,68 @@
-import { Button } from "@mui/material";
-import { useState } from "react";
-
-interface Person {
-  name: string
-}
+import { Box, Button, Typography } from "@mui/material";
+import { Link } from "react-router";
 
 export default function Home() {
-
-  const [person, setPerson] = useState<Person>({name:'Sergio'});
-
-  // const handleClick: Person = () => {
-  //   setPerson({
-  //     ...person,
-  //     name: 'Luis!'
-  //   })
-  // }
-
   return (
     <>
-    <div>{person.name}</div>
-    <Button color="error" onClick={() => setPerson({ ...person, name: 'Inline name' })}>Update Inline</Button>
-    {/* <Button color="error" onClick={handleClick}>Update</Button> */}
+      <Box maxWidth="xl" mx="auto" px={4} position="relative">
+        <Box
+          display="flex"
+          flexDirection="column"
+          alignItems="center"
+          justifyContent="center"
+          position="relative"
+        >
+          <img
+            src="/images/hero1.jpg"
+            alt="ski resort image"
+            style={{
+              position: "absolute",
+              inset: 0,
+              width: "100%",
+              height: "100%",
+              objectFit: "cover",
+              borderRadius: "16px",
+              zIndex: 0,
+            }}
+          />
+          <Box
+            display="flex"
+            flexDirection="column"
+            p={8}
+            alignItems="center"
+            position="relative"
+            borderRadius={4}
+          >
+            <Typography
+              variant="h1"
+              color="white"
+              fontWeight="bold"
+              textAlign="center"
+              sx={{ my: 3 }}
+            >
+              Welcome to Restore!
+            </Typography>
+            <Button
+              variant="contained"
+              size="large"
+              component={Link}
+              to="/catalog"
+              sx={{
+                mt: 8,
+                backgroundImage: "linear-gradient(to right, #2563eb, #06b6d4)",
+                fontWeight: "bold",
+                color: "white",
+                borderRadius: "16px",
+                px: 8,
+                py: 2,
+                border: "2px solid transparent",
+              }}
+            >
+              View Catalog
+            </Button>
+          </Box>
+        </Box>
+      </Box>
     </>
-  )
+  );
 }
